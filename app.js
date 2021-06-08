18e1dff0eb1173ac3b49cb4e61bbafa0b13302d1
unknown <30985@fiecalunos.lan>
2021-06-08 14:31:10 -0300

    Adição do arquivo HTML

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/app'))
+
+app.set('views', './views')
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 app.get('/', (req,res) => {
     res.status(200).send({ message : 'Deu tudo certo!' });
 })
 
 app.get('/teste', (req,res) => {
-    res.send({ message : 'Deu tudo certo!' });
+    res.render('index.html')
 })
 
 app.listen(8080, () => {
