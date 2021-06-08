522cb664f47c40640ad2e6b1e9fd246b9af8d60a
AlexManoel-dev <alex30985@fiec.edu.br>
2021-06-08 20:29:49 -0300

    CSS funcionando!

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
+const path = require('path')
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
-app.use(express.static(__dirname + '/app'))
 
-app.set('views', './views')
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
+// app.use(express.static(__dirname + '/app'))
+// app.set('views', './views')
+// app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, "public" )));
+app.set("views", path.join(__dirname, "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
 app.get('/', (req,res) => {
-    res.status(200).send({ message : 'Deu tudo certo!' });
+    res.status(200).send({ message : 'Deu tudo certo'})
 })
 
 app.get('/teste', (req,res) => {
-    res.render('index.html')
+    res.render('views/index.html')
 })
 
 app.listen(8080, () => {
